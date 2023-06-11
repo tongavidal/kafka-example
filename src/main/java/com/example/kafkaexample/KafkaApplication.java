6e2d529794d0f2473ee43746002d4bcb3c459b1e
Gaston Andres Vidal <vidalgaston92@gmail.com>
2023-06-11 11:56:57 +0200

    Add KafkaConsumerConfig

--- src/main/java/com/example/kafkaexample/KafkaApplication.java
+++ src/main/java/com/example/kafkaexample/KafkaApplication.java
@@ -15,7 +15,10 @@ public class KafkaApplication {
 
 	@Bean
 	CommandLineRunner commandLineRunner(KafkaTemplate<String, String> kafkaTemplate){
-		return args -> {kafkaTemplate.send("gastonTopic", "test message 2");
+		return args -> {
+			for (int i = 0; i < 100; i++) {
+				kafkaTemplate.send("gastonTopic", "test message nº" + i);
+			}
 		};
 	}
 
